c97142428aff41ade854e4d2934f7295faeee28b
Ryan Kann <ryanakann@gmail.com>
2022-03-08 20:59:20 -0500

    Update UnitUI.cs

--- Clown Town/Assets/Encounters/Units/UnitUI.cs
+++ Clown Town/Assets/Encounters/Units/UnitUI.cs
@@ -23,20 +23,25 @@ namespace Encounters
             _unitInfo = unitInfo;
 
             _unitInfo.GetComponent<UnitBehavior>().OnHealthChanged?.AddListener(UpdateHealth);
+            SetHealth(_unitInfo.MaxHealth);
         }
 
         public void SetHealth(float health)
         {
             _greenBar.rectTransform.localScale = new Vector3(health / _unitInfo.MaxHealth, 1f, 1f);
             _orangeBar.rectTransform.localScale = new Vector3(health / _unitInfo.MaxHealth, 1f, 1f);
+            _canvas.enabled = false;
         }
 
         private bool _hitInProgress = false;
         private float _targetPercentage = 0f;
         private const float _hitDuration = 2f;
+        private float _timeSpentWaiting = 0f;
 
         public void UpdateHealth(float health)
         {
+            _canvas.enabled = true;
+            _timeSpentWaiting = 0f;
             _targetPercentage = health / _unitInfo.MaxHealth;
             _greenBar.rectTransform.localScale = new Vector3(_targetPercentage, 1f, 1f);
 
@@ -59,6 +64,18 @@ namespace Encounters
             }
             _orangeBar.rectTransform.localScale = new Vector3(_targetPercentage, 1f, 1f);
             _hitInProgress = false;
+
+            while (_timeSpentWaiting < 2f)
+            {
+                if (_hitInProgress) break;
+                _timeSpentWaiting += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+
+            if (!_hitInProgress)
+            {
+                _canvas.enabled = false;
+            }
         }
     }
 }
